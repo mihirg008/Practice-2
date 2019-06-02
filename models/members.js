db241431f5d0cedf802c4308777c6823328f4ce2
mihirg008 <mihirg008@gmail.com>
2019-06-02 13:49:15 +0530

    about us and initiatives update

---
+++ models/members.js
@@ -0,0 +1,9 @@
+const mongoose = require('mongoose');
+mongose.Promise = global.Promise;
+cont slug = require('slugs');
+
+cont memberSchema = new mongoose.Schema({
+
+});
+
+module.exports = mongoose.model('Members', memberSchema)
--- routes/index.js
+++ routes/index.js
@@ -4,7 +4,35 @@ var router = express.Router();
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Gopali Youth Welfare Society',
-                        top_image: 'header-bg.jpg'});
+                        top_image: 'header-bg.jpg',
+                        atag:'#about-uss'});
 });
 
+/* GET about-us page. */
+router.get('/about', function(req, res, next) {
+  res.render('about', { title: 'GYWS | About Us',
+                        top_image: 'header-bg.jpg',
+                        atag:'about#about-uss'});
+});
+
+/* GET jvm page. */
+router.get('/jvm', function(req, res, next) {
+  res.render('jvm', { title: 'GYWS | JVM',
+                        top_image: 'header-bg.jpg',
+                        atag:'about#about-uss'});
+});
+
+/* GET prayas page. */
+router.get('/prayas', function(req, res, next) {
+  res.render('prayas', { title: 'GYWS | Prayas',
+                        top_image: 'header-bg.jpg',
+                        atag:'about#about-uss'});
+});
+
+/* GET udyat page. */
+router.get('/udyat', function(req, res, next) {
+  res.render('udyat', { title: 'GYWS | Udyat',
+                        top_image: 'header-bg.jpg',
+                        atag:'about#about-uss'});
+});
 module.exports = router;
